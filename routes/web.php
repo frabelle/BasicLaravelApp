db3dc0e8fac7d1fb6220d9c3976140c14303cf85
frabelle <renchifrabelle@gmail.com>
2022-02-11 23:52:51 -0600

    migration to db ready

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('productos', 'App\Http\Controllers\ProductosController');
